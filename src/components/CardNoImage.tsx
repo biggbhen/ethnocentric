a831e6e1e4ba32fd1d9d5df5eeee8ffe644b9753
chidi <chididaniel1998@gmail.com>
2025-01-22 10:52:28 +0100

    refactor CardNoImage component layout for improved structure and styling

--- src/components/CardNoImage.tsx
+++ src/components/CardNoImage.tsx
@@ -8,13 +8,16 @@ interface CardProps {
 
 const CardNoImage: React.FC<CardProps> = ({ title, des, color }) => {
   return (
-    <div className={`text-3xl font-bold h-fit md:h-72 w-full mx-auto flex items-center justify-start text-[${color}] p-4 overflow-hidden`}>
+    <div className={`text-3xl font-bold h-fit md:h-72 w-full flex items-center justify-start text-[${color}] p-4 overflow-hidden`}>
       <div
-        className="z-10 text-left"
+        className="z-10 text-left w-full  flex md:justify-center "
        
       >
-        <h3 className="text-2xl font-bold mb-2">{title}</h3>
+        <div>
+           <h3 className="text-2xl font-bold mb-2">{title}</h3>
         <p className="text-lg">{des}</p>
+        </div>
+       
       </div>
     </div>
   );
